1918dd381e70a1997929d5d063f3a6a6a6688edd
Mohammadreza-Tatlari <mohammadreza.tatlari8@gmail.com>
2023-11-02 17:35:21 +0330

    category file can now read from URL and highlight the related category when it is selected. also Logo can reset path as well.

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,14 +1,15 @@
 "use client";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Logo() {
 
 //this router may get changes to link component
-//const router = useRouter();
+const router = useRouter();
 
   return (
-    <div className="flex items-center">
-      <span className="self-center text-2xl font-semibold whitespace-nowrap ">
+    <div className="flex items-center hover:cursor-pointer"
+    onClick={() => router.push('/')}>
+      <span className="rounded-full p-1 text-slate-950 self-center text-2xl font-semibold whitespace-nowrap hover:shadow-md ">
         WeHouse
       </span>
     </div>
