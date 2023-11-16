5a9a0dcb8486e2980741b9efb4e692bb5f64e011
Mohammadreza-Tatlari <mohammadreza.tatlari8@gmail.com>
2023-11-16 14:15:55 +0330

    due to dynamic server rendering force-dynamic is added to page.tsx for build time

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+// 'automatic' | 'dynamic force' | 'error' | 'static force'
 
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings, { IListingsProps } from "./actions/getListings";
@@ -7,25 +9,26 @@ import Container from "./components/Container";
 import EmpyState from "./components/EmpyState";
 import ListingCard from "./components/Listing/ListingCard";
 
-interface HomeProps{
-  searchParams: IListingsProps
+interface HomeProps {
+  searchParams: IListingsProps;
 }
 //async for listing action
-export default async function Home({searchParams}:HomeProps) {
+
+const Home = async ({ searchParams }: HomeProps) => {
+  //export default async function Home({searchParams}:HomeProps) {
 
   //working with currentUser so no API call to database is needed
   //because it is serverside rendering and using database directly
-  const listings = await getListings(searchParams) //searchParams is an Object
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
   //if content is not selected
   //using ClientOnly for hydration
-  if(listings.length === 0) {
-    return(
+  if (listings.length === 0) {
+    return (
       <ClientOnly>
-        <EmpyState 
-        showReset />
+        <EmpyState showReset />
       </ClientOnly>
-    )
+    );
   }
 
   return (
@@ -33,20 +36,22 @@ export default async function Home({searchParams}:HomeProps) {
       <ClientOnly>
         <Container>
           <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 ">
-           {listings.map((listing) => {
-            return(
-              <>
-              <ListingCard
-              key={listing.id}
-              data={listing}
-              currentUser={currentUser} 
-              />
-              </>
-            )
-           })}
+            {listings.map((listing) => {
+              return (
+                <>
+                  <ListingCard
+                    key={listing.id}
+                    data={listing}
+                    currentUser={currentUser}
+                  />
+                </>
+              );
+            })}
           </div>
         </Container>
       </ClientOnly>
     </>
   );
-}
+};
+
+export default Home;
